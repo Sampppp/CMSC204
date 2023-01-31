c09deea5f17d335b0a25d75425faa664b8de04c8
Sampppppppppppppppppppppppp <legosamson@gmail.com>
2023-01-31 17:40:27 -0500

    like halfway done

--- Assignment1/Assignment1/src/PasswordCheckerUtility.java
+++ Assignment1/Assignment1/src/PasswordCheckerUtility.java
@@ -2,9 +2,8 @@ import jave.util.*;
 public class PasswordCheckerUtility {
     private String password;
     private String passwordConfirm;
-    private ArrayList<String> passwords = new ArrayList<String>();
+    private ArrayList<String> passwords = new ArrayList<>();
     
-    //add 'throws' to methods
     //finish proj idk
     
     public PasswordChecherUtility(){
@@ -20,10 +19,11 @@ public class PasswordCheckerUtility {
 
     }
 
-    public static boolean isValidLength(String password){
+    public static boolean isValidLength(String password)throws LengthException{
         return (password.length() < 6);
     }
-    public static boolean hasUpperAlpha(String password){
+
+    public static boolean hasUpperAlpha(String password)throws NoUpperAlphaException{
         for(int i = 0; i < password.length(); i++){
             if(Character.isUpperCase(password.charAt(i))){
                 return true;
@@ -32,7 +32,7 @@ public class PasswordCheckerUtility {
         return false;
     }
 
-    public static boolean hasLowerAlpha(String password){
+    public static boolean hasLowerAlpha(String password)throws NoLowerAlphaException{
         for(int i = 0; i < password.length(); i++){
             if(Character.isLowerCase(password.charAt(i))){
                 return true;
@@ -41,7 +41,7 @@ public class PasswordCheckerUtility {
         return false;
     }
     
-    public static boolean hasDigit(String password){
+    public static boolean hasDigit(String password)throws NoDigitException{
         for(int i = 0; i < password.length; i++){
             if(Character.isDigit(password.charAt(i))){
                 return true;
@@ -50,7 +50,7 @@ public class PasswordCheckerUtility {
         return false;
     }
 
-    public static boolean hasSpecialChar(String password){
+    public static boolean hasSpecialChar(String password)throws NoSpecialCharacterException{
         for(int i = 0; i < password.length; i++){
             if(!Character.isLetter(password.charAt(i)) && Character.isDigit(password.charAt(i)))
                 return true;   
@@ -58,7 +58,7 @@ public class PasswordCheckerUtility {
         return false;
     }
 
-    public static boolean NoSameCharInSequence(String password){
+    public static boolean noSameCharInSequence(String password)throws InvalidSequenceException{
         for(int i = 1; i < password.length - 1; i++){
             if(password.charAt(i) == password.charAt(i - 1) && password.charAt(i) == password.charAt(i + 1))
                 return false;
@@ -66,20 +66,20 @@ public class PasswordCheckerUtility {
         return true;
     }
 
-    public static boolean isValidPassword(String password){
-        
+    public static boolean isValidPassword(String password)throws LengthException, NoUpperAlphaException, NoLowerAlphaException, NoDigitException, NoSpecialCharacterException, InvalidSequenceException{
+        return (isValidLength(password) && hasUpperAlpha(password) && hasLowerAlpha(password) && hasDigit(password) && hasSpecialChar(password) && noSameCharInSequence(password));
     }
 
     public static boolean hasBetweenSixAndNineChars(String password){
         return (password.length() > 5 && password.length() < 10);
     }
 
-    public static boolean isWeakPassword(String password){
+    public static boolean isWeakPassword(String password)throws WeakPasswordException{
         return (isValidLength(password) && hasBetweenSixAndNineChars(password));
     }
 
     public static ArrayList<String> getInvalidPasswords(ArrayList<String> passwords){
-        
+
     }
 
 }
