643ee27b6422f541e331a94e60f52eb57ff60499
Sampppppppppppppppppppppppp <legosamon@gmail.com>
2023-04-05 11:22:04 -0400

    I mean it passes Junit...

--- Assignment5/src/MorseCodeConverter.java
+++ Assignment5/src/MorseCodeConverter.java
@@ -1,22 +1,41 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MorseCodeConverter {
-
+	private static MorseCodeTree tree = new MorseCodeTree();
 	//constructor
 	public MorseCodeConverter() {
 	}
 	
 	public static String printTree(){
-		
+		ArrayList<String> temp = tree.toArrayList();
+		String print = "";
+		for(int i = 0; i < temp.size(); i++) {
+			print += temp.get(i) + " ";
+		}
+		return print.substring(0, print.length() - 2);
 	}
 	
 	
 	public static String convertToEnglish(String code) {
-		
+		String[] temp = code.split(" ");
+		String result = "";
+		for(int i = 0; i < temp.length; i++) {
+			if(temp[i].compareTo("/") == 0) 
+				result += " ";
+			else
+				result += tree.fetch(temp[i]);
+		}
+		return result;
 	}
 	
 	public static String convertToEnglish(File codeFile) throws java.io.FileNotFoundException {
+		Scanner in = new Scanner(codeFile);
+		String result = convertToEnglish(in.nextLine());
+		in.close();
 		
+		return result;
 	}
 	
 	
--- Assignment5/src/MorseCodeTree.java
+++ Assignment5/src/MorseCodeTree.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 
 public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
-	TreeNode<String> tree;
 	TreeNode<String> root;
 	
 	//constructor
 	MorseCodeTree(){
 		buildTree();
 	}
-	
+	//copy constructor
+	MorseCodeTree(MorseCodeTree a){
+		root = new TreeNode<>(a.root);
+	}
 	
 	
 	@Override
@@ -44,54 +46,99 @@ public class MorseCodeTree implements LinkedConverterTreeInterface<String> {
 		switch(code.charAt(0)){
 		case '.':
 			root.setLeft(temp);
-			addNode(temp, code.substring(1), letter + ".");
+			addNode(temp, code.substring(1), letter);
 			break;
 		case '-':
 			root.setRight(temp);
-			addNode(temp, code.substring(1), letter + "-");
+			addNode(temp, code.substring(1), letter);
 			break;
 		}
 	}
 
 	@Override
 	public String fetch(String code) {
-		// TODO Auto-generated method stub
-		return null;
+		return fetchNode(root, code);
 	}
 
 	@Override
 	public String fetchNode(TreeNode<String> root, String code) {
-		// TODO Auto-generated method stub
+		if(code.length() == 1) {
+			switch(code.charAt(0)){
+			case '.':
+				return root.getLeft().getData();
+			case '-':
+				return root.getRight().getData();
+			}
+		}
+			
+		switch(code.charAt(0)){
+		case '.':
+			return fetchNode(root.getLeft(), code.substring(1));
+		case '-':
+			return fetchNode(root.getRight(), code.substring(1));
+		}
 		return null;
 	}
 
 	@Override
-	public LinkedConverterTreeInterface<String> delete(String data) throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+	public MorseCodeTree delete(String data) throws UnsupportedOperationException {
+		MorseCodeTree temp = new MorseCodeTree(this);
+		root = null;
+		return temp;
 	}
 
 	@Override
-	public LinkedConverterTreeInterface<String> update() throws UnsupportedOperationException {
-		// TODO Auto-generated method stub
-		return null;
+	public MorseCodeTree update() throws UnsupportedOperationException {
+		return this;
 	}
 
 	@Override
 	public void buildTree() {
-		tree = new TreeNode<>("");
-		root = tree;
+		root = new TreeNode<>("");
+		
+		insert(".", "e");
+		insert("-", "t");
+		insert("..", "i");
+		insert(".-", "a");
+		insert("-.", "n");
+		insert("--", "m");
+		insert("", "s");
+		insert("", "u");
+		insert("", "r");
+		insert("", "w");
+		insert("", "d");
+		insert("", "k");
+		insert("", "g");
+		insert("", "o");
+		insert("", "h");
+		insert("", "v");
+		insert("", "f");
+		insert("", "l");
+		insert("", "p");
+		insert("", "j");
+		insert("", "b");
+		insert("", "x");
+		insert("", "c");
+		insert("", "y");
+		insert("", "z");
+		insert("", "q");
 	}
 
 	@Override
 	public ArrayList<String> toArrayList() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<String> list = new ArrayList<>();
+		LNRoutputTraversal(root, list);
+		return list;
 	}
 
 	@Override
 	public void LNRoutputTraversal(TreeNode<String> root, ArrayList<String> list) {
+		if(root == null)
+			return;
 		
+		LNRoutputTraversal(root.getLeft(), list);
+		list.add(root.getData());
+		LNRoutputTraversal(root.getRight(), list);
 	}
 
 
