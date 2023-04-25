eab719caeb395ffac7d0c1f71fc6ea50006395dd
Sampppppppppppppppppppppppp <legosamson@gmail.com>
2023-04-25 17:38:01 -0400

    forgot upload

---
+++ Assingnment6/src/Road.java
@@ -0,0 +1,10 @@
+
+public class Road implements Comparable<Road>{
+
+	@Override
+	public int compareTo(Road o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- Assingnment6/src/Town.java
+++ Assingnment6/src/Town.java
@@ -1,4 +1,10 @@
 
-public class Town {
+public class Town implements Comparable<Town>{
+
+	@Override
+	public int compareTo(Town o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
